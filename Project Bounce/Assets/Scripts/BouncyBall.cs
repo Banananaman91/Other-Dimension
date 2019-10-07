6dcf39bc19cf97c039626b28ff2d5a951cae1737
Ian Barrow <Barrowian1991@gmail.com>
2019-10-07 15:52:06 +0100

    Finished movement, added camera rotation for object
    Current bug - Camera flashing in viewport, caused by rapidly switching opposite positions around object. Unclear what is causing it

--- Project Bounce/Assets/Scripts/BouncyBall.cs
+++ Project Bounce/Assets/Scripts/BouncyBall.cs
@@ -8,35 +8,29 @@ using UnityEngine.Assertions.Comparers;
 public class BouncyBall : MonoBehaviour
 {
     [SerializeField] private Rigidbody _rb;
+    [SerializeField] private Camera _camera;
     [SerializeField] private float _bounce;
     [SerializeField] private float _directionForce;
-    [SerializeField] private float _rotationForce;
+    [SerializeField] private float clampDistance;
     private Vector3 _moveDirection = Vector3.zero;
-    private Vector3 _rotationDirection = Vector3.zero;
     private Vector3 _directionVector = Vector3.zero;
-    
-
-    private void Start()
-    {
-        
-    }
+    private Vector3 MyTransform => transform.position;
+    private Vector3 CameraTransform => _camera.transform.position;
+    private Transform RB => _rb.transform;
 
     private void Update()
     {
         _moveDirection = Vector3.zero;
-        _rotationDirection = Vector3.zero;
         _moveDirection.x = Input.GetAxis("Horizontal");
         _moveDirection.z = Input.GetAxis("Vertical");
-        if (_moveDirection != Vector3.zero) transform.forward = _moveDirection;
-        
         if (Input.GetKeyDown(KeyCode.Space)) _rb.AddForce(Vector3.down * Mathf.Sqrt(_bounce * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+        transform.rotation = Quaternion.Euler(0, _camera.transform.eulerAngles.y, 0);
+        _camera.transform.position = MyTransform + ((MyTransform - CameraTransform).normalized * clampDistance);
     }
 
     private void FixedUpdate()
     {
-        _directionVector = (_rb.transform.right * _moveDirection.x) + (_rb.transform.forward * _moveDirection.z);
-        _rb.MovePosition(_rb.position + Time.fixedDeltaTime * _directionForce * _directionVector);
-//        Quaternion deltaRotation = Quaternion.Euler(_rotationForce * Time.deltaTime * _rotationDirection);
-//        _rb.MoveRotation(_rb.rotation * deltaRotation);
+        _directionVector = (RB.right * _moveDirection.x) + (RB.forward * _moveDirection.z);
+        _rb.MovePosition(RB.position + Time.fixedDeltaTime * _directionForce * _directionVector);
     }
 }
---
+++ Project Bounce/Assets/Scripts/CameraOrbit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour
+{ 
+    [SerializeField] private Transform _target; 
+    [SerializeField] private float _rotateSpeed;
+
+    void LateUpdate()
+     {
+         if(Input.GetKey (KeyCode.E))
+         {
+             transform.RotateAround(_target.position, Vector3.up, _rotateSpeed * Time.deltaTime);
+         }
+
+         if(Input.GetKey (KeyCode.Q))
+         {
+             transform.RotateAround(_target.position, -Vector3.up, _rotateSpeed * Time.deltaTime);
+         }
+     }
+}
