65971363034a595cb961792e741264f70f11a37c
Ian <barrowian1991@gmail.com>
2019-10-11 13:52:24 +0100

    Added bounce reduction with height check
    Needs refining, possibly check height distance via raycast to ground

--- Project Bounce/Assets/Scripts/CameraOrbit.cs
+++ Project Bounce/Assets/Scripts/CameraOrbit.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class CameraOrbit : MonoBehaviour
 { 
-    [SerializeField] private Transform _target; 
+    [SerializeField] private Transform _target;
+    [SerializeField] private Transform _clamp;
     [SerializeField] private float _rotateSpeed;
 
     void LateUpdate()
@@ -19,5 +20,7 @@ public class CameraOrbit : MonoBehaviour
          {
              transform.RotateAround(_target.position, -Vector3.up, _rotateSpeed * Time.deltaTime);
          }
+         
+         transform.position = _clamp.position;
      }
 }
