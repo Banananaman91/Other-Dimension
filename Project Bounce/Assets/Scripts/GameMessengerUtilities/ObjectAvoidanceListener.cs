2a5d487e8c9c8ca70496c93ab59cf32a8f1856e9
Ian <barrowian1991@gmail.com>
2019-10-31 14:18:03 +0000

    Added A* pathfinder
    Added my own A* pathfinder with relevant interfaces and message broker system

---
+++ Project Bounce/Assets/Scripts/GameMessengerUtilities/ObjectAvoidanceListener.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace GameMessengerUtilities
+{
+    public class ObjectAvoidanceListener : MonoBehaviour
+    {
+        private ObjectAvoidance _avoidanceInstance;
+
+        private void Awake()
+        {
+            MessageBroker.Instance.RegisterMessageOfType<ObjectRequestMessage>(OnObjectAvoidanceRequestMessage);
+            _avoidanceInstance = new ObjectAvoidance();
+        }
+
+        private void OnObjectAvoidanceRequestMessage(ObjectRequestMessage message) => message.RequestingComponent.ObjectInitialise(_avoidanceInstance);
+    }
+}
