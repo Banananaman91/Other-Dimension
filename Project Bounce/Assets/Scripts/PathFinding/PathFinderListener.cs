bd8db29f94c3c25ec8df191b4f5d344d754f76d4
Ian Barrow <43844902+Banananaman91@users.noreply.github.com>
2019-11-14 12:38:34 +0000

    Revert "Merge pull request #8 from Banananaman91/development"
    This reverts commit f810c1cc995515d1adeac136c61323dfdcd4568d.

--- Project Bounce/Assets/Scripts/PathFinding/PathFinderListener.cs
+++ Project Bounce/Assets/Scripts/PathFinding/PathFinderListener.cs
@@ -5,7 +5,7 @@ namespace PathFinding
 {
     public class PathFinderListener : MonoBehaviour
     {
-        private void Awake() => GameMessengerUtilities.MessageBroker.Instance.RegisterMessageOfType<PathFinderRequestMessage>(OnPathFinderComponentRequestMessage);
+        private void Awake() => MessageBroker.Instance.RegisterMessageOfType<PathFinderRequestMessage>(OnPathFinderComponentRequestMessage);
         
         private void OnPathFinderComponentRequestMessage(PathFinderRequestMessage message) => message.RequestingComponent.PathInitialise(new PathFinder());
     }
