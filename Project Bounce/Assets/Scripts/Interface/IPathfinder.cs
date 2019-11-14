9fe836bdd6adbd943699302ad1c23d82615d1986
Ian Barrow <43844902+Banananaman91@users.noreply.github.com>
2019-11-14 12:43:09 +0000

    Revert "Merge pull request #9 from Banananaman91/feature/ai-movement"
    This reverts commit 2387b20afa746ece043ae1d0d4b20a09ea7ab52e.

--- Project Bounce/Assets/Scripts/Interface/IPathfinder.cs
+++ Project Bounce/Assets/Scripts/Interface/IPathfinder.cs
@@ -7,6 +7,6 @@ namespace Interface
 {
     public interface IPathfinder
     {
-        IEnumerator FindPath(Vector3 startPosition, Vector3 targetPosition, float movementRadius, Action<IEnumerable<Vector3>> onCompletion);
+        IEnumerator FindPath(Vector3 startPosition, Vector3 targetPosition, bool is3d, float movementRadius, Action<IEnumerable<Vector3>> onCompletion);
     }
 }
