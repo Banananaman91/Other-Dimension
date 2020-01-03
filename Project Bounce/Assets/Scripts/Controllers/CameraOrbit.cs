f2da4a6fc8b295510859d382096d0070821b9f8a
Ian Barrow <Barrowian1991@gmail.com>
2020-01-03 21:24:08 +0000

    Fixed camera to follow

--- Project Bounce/Assets/Scripts/Controllers/CameraOrbit.cs
+++ Project Bounce/Assets/Scripts/Controllers/CameraOrbit.cs
@@ -1,27 +1,25 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Controllers
 {
     public class CameraOrbit : MonoBehaviour
     { 
-        [SerializeField] private Transform _target;
-        [SerializeField] private Transform _clamp;
-        [SerializeField] private float _rotateSpeed;
-        private Vector3 Clamp => transform.position - _target.position;
-
-        void LateUpdate()
+        [SerializeField] private GameObject _target;
+        [SerializeField] private int _heightAbovePlayer;
+        [SerializeField] private float _upDistance;
+        [SerializeField] private float _backDistance;
+        [SerializeField] private float _trackingSpeed;
+        private Vector3 _targetVector3;
+        private Quaternion _targetQuaternion;
+        private Transform TargetTransform => _target.transform;
+        private Vector3 _aboveVector3;
+        private void LateUpdate()
         {
-//            if(Input.GetKey (KeyCode.E))
-//            {
-//                transform.RotateAround(_target.position, Vector3.up, _rotateSpeed * Time.deltaTime);
-//            }
-//
-//            if(Input.GetKey (KeyCode.Q))
-//            {
-//                transform.RotateAround(_target.position, -Vector3.up, _rotateSpeed * Time.deltaTime);
-//            }
-            transform.position = _clamp.position;
-            transform.LookAt(_target, _target.up);
+            _targetVector3 = TargetTransform.position - TargetTransform.forward * _backDistance + TargetTransform.up * _upDistance;
+            _aboveVector3 = TargetTransform.position + TargetTransform.up * _upDistance;
+            transform.position = Vector3.Lerp (transform.position, _targetVector3, _trackingSpeed * Time.deltaTime);
+            transform.LookAt(_aboveVector3, TargetTransform.up);
         }
     }
 }
