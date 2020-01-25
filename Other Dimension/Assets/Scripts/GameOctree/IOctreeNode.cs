9edec94326c1f4f63199ad8e13b1336e89edd1bd
Ian Barrow <Barrowian1991@gmail.com>
2020-01-25 13:02:40 +0000

    Auto stash before merge of "feature/octree" and "origin/feature/octree"

--- Other Dimension/Assets/Scripts/GameOctree/IOctreeNode.cs
+++ Other Dimension/Assets/Scripts/GameOctree/IOctreeNode.cs
@@ -4,9 +4,11 @@ namespace GameOctree
 {
     public interface IOctreeNode
     {
-        Bounds BoundingArea { get; set; }
-        Bounds Region { get; set; }
-        IOctreeNode[] ChildNodes { get; set; }
-        
+        //Objects contained within the node
+        //private List<T> _objects;
+        // bounding box area
+        //private Bounds _region;
+        //Octree centre
+        //private Vector3 _centre = new Vector3(0, 0, 0);
     }
 }
