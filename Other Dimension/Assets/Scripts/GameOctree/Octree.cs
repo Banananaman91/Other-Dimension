225899385edcfa2a1516505410b31989531513d6
Ian Barrow <Barrowian1991@gmail.com>
2020-01-26 11:59:56 +0000

    First Octree implementation testing

--- Other Dimension/Assets/Scripts/GameOctree/Octree.cs
+++ Other Dimension/Assets/Scripts/GameOctree/Octree.cs
@@ -1,54 +1,151 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace GameOctree
 {
-    public class Octree<T> where T : IOctreeNode, new()
+    public enum OctreeIndex
     {
-        private T _parent;
-        private List<T> _objects;
-        private Bounds _region;
-        private int _minSize = 1;
-        private bool _treeReady = false;
-        private bool _treeBuilt = false;
-
-        private Bounds[] CreateBounds(Vector3 min, Vector3 max) //create bounds array for checking
+        LowerLeftFront = 0, //000,
+        LowerRightFront = 2, //010,
+        LowerRightBack = 3, //011,
+        LowerLeftBack = 1, //001,
+        UpperLeftFront = 4, //100,
+        UpperRightFront = 6, //110,
+        UpperRightBack = 7, //111,
+        UpperLeftBack = 5, //101,
+    }
+    public class Octree<T>
+    {
+        private OctreeNode<T> _node;
+        private int _depth;
+        
+        public Octree(Vector3 position, float size, int depth)
         {
-            var bounds = new Bounds[8];
-            var half = (max - min) / 2;
-            var centre = min + half;
-            //Create 8 bounding boxes inside original bounding box
-            bounds[0] = new Bounds(min, centre);
-            bounds[1] = new Bounds(new Vector3(centre.x, min.y, min.z), new Vector3(max.x, centre.y, centre.z));
-            bounds[2] = new Bounds(new Vector3(centre.x, min.y, centre.z), new Vector3(max.x, centre.y, max.z));
-            bounds[3] = new Bounds(new Vector3(min.x, min.y, centre.z), new Vector3(centre.x, centre.y, max.z));
-            bounds[4] = new Bounds(new Vector3(min.x, centre.y, min.z), new Vector3(centre.x, max.y, centre.z));
-            bounds[5] = new Bounds(new Vector3(centre.x, centre.y, min.z), new Vector3(max.x, max.y, centre.z));
-            bounds[6] = new Bounds(centre, max);
-            bounds[7] = new Bounds(new Vector3(min.x, centre.y, centre.z), new Vector3(centre.x, max.y, max.z));
-            return bounds;
-        }
-
-        private void Insert(T target, List<T> updateList, Vector3 min, Vector3 max)
-        {
-            var bounds = CreateBounds(min, max); // create array of bounding boxes to be checked against
+            _node = new OctreeNode<T>(position, size);
+            _node.SubDivide(depth);
             
-            for (int i = 0; i < bounds.Length; i++) // for all the bounds
+        }
+        
+        public class OctreeNode<T>
+        {
+            private OctreeNode<T>[] _subNodes;
+            private IList<T> value;
+            private Vector3 _position;
+            private float _size;
+
+            public OctreeNode(Vector3 pos, float size)
             {
-                foreach (var octreeNode in target.ChildNodes)// check the children of target node
+                _position = pos;
+                _size = size;
+            }
+
+            public IEnumerable<OctreeNode<T>> Nodes => _subNodes;
+            public Vector3 Position => _position;
+            public float Size => _size;
+
+            public void SubDivide(int depth = 1)
+            {
+                _subNodes = new OctreeNode<T>[8];
+                for (int i = 0; i < _subNodes.Length; i++)
                 {
-                    var child = (T) octreeNode;
-                    if (!bounds[i].Contains(child.BoundingArea.size)) continue; // if the bounds doesn't contain the child, continue
-                    child.Region = bounds[i];
-                    if (child.ChildNodes == null) // if the child has no children, it is an end node.
+                    Vector3 newPos = _position;
+                    if ((i & 4) == 4)
                     {
-                        updateList.Add(child);
-                        continue;
+                        newPos.y += _size * 0.25f;
                     }
-                    
-                    Insert(child, updateList, bounds[i].min, bounds[i].max); // Recursive call down through child nodes
+                    else
+                    {
+                        newPos.y -= _size * 0.25f;
+                    }
+                    if ((i & 2) == 2)
+                    {
+                        newPos.x += _size * 0.25f;
+                    }
+                    else
+                    {
+                        newPos.x -= _size * 0.25f;
+                    }
+                    if ((i & 1) == 1)
+                    {
+                        newPos.z += _size * 0.25f;
+                    }
+                    else
+                    {
+                        newPos.z -= _size * 0.25f;
+                    }
+                    _subNodes[i] = new OctreeNode<T>(newPos, _size * 0.5f);
+                    if (depth > 0)
+                    {
+                        _subNodes[i].SubDivide(depth - 1);
+                    }
                 }
             }
+
+            public bool IsLeaf()
+            {
+                return _subNodes == null;
+            }
         }
+
+        private int GetIndexOfPosition(Vector3 lookupPosition, Vector3 nodePosition)
+        {
+            var index = 0;
+
+            index |= lookupPosition.y > nodePosition.y ? 4 : 0;
+            index |= lookupPosition.x > nodePosition.x ? 2 : 0;
+            index |= lookupPosition.z > nodePosition.z ? 1 : 0;
+            
+            return index;
+        }
+        
+        public OctreeNode<T> GetRoot()
+        {
+            return _node;
+        }
+        
+        // private Octree _parent;
+        // private List<IOctreeNode> _objects;
+        // private Octree[] _childNodes = new Octree[8];
+        // private Bounds _region;
+        //
+        // private Bounds[] CreateBounds(Vector3 min, Vector3 max) //create bounds array for checking
+        // {
+        //     var bounds = new Bounds[8];
+        //     var half = (max - min) / 2;
+        //     var centre = min + half;
+        //     //Create 8 bounding boxes inside original bounding box
+        //     bounds[0] = new Bounds(min, centre);
+        //     bounds[1] = new Bounds(new Vector3(centre.x, min.y, min.z), new Vector3(max.x, centre.y, centre.z));
+        //     bounds[2] = new Bounds(new Vector3(centre.x, min.y, centre.z), new Vector3(max.x, centre.y, max.z));
+        //     bounds[3] = new Bounds(new Vector3(min.x, min.y, centre.z), new Vector3(centre.x, centre.y, max.z));
+        //     bounds[4] = new Bounds(new Vector3(min.x, centre.y, min.z), new Vector3(centre.x, max.y, centre.z));
+        //     bounds[5] = new Bounds(new Vector3(centre.x, centre.y, min.z), new Vector3(max.x, max.y, centre.z));
+        //     bounds[6] = new Bounds(centre, max);
+        //     bounds[7] = new Bounds(new Vector3(min.x, centre.y, centre.z), new Vector3(centre.x, max.y, max.z));
+        //     return bounds;
+        // }
+        //
+        // private void Insert(Octree target, List<Octree> updateList, Vector3 min, Vector3 max)
+        // {
+        //     var bounds = CreateBounds(min, max); // create array of bounding boxes to be checked against
+        //     
+        //     for (int i = 0; i < bounds.Length; i++) // for all the bounds
+        //     {
+        //         foreach (var octreeNode in _childNodes)// check the children of target node
+        //         {
+        //             if (!bounds[i].Contains(octreeNode._region.size)) continue; // if the bounds doesn't contain the child, continue
+        //             octreeNode._region = bounds[i];
+        //             if (octreeNode._childNodes == null) // if the child has no children, it is an end node.
+        //             {
+        //                 updateList.Add(octreeNode);
+        //                 continue;
+        //             }
+        //             
+        //             Insert(octreeNode, updateList, bounds[i].min, bounds[i].max); // Recursive call down through child nodes
+        //         }
+        //     }
+        // }
+
     }
 }
---
+++ Other Dimension/Assets/Scripts/GameOctree/OctreeComponent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+namespace GameOctree
+{
+    public class OctreeComponent : MonoBehaviour
+    {
+        public float size = 5;
+
+        public int depth = 2;
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        private void OnDrawGizmos()
+        {
+            var octree = new Octree<int>(this.transform.position, size, depth);
+            DrawNode(octree.GetRoot());
+        }
+
+        private void DrawNode(Octree<int>.OctreeNode<int> node)
+        {
+            if (node.IsLeaf())
+            {
+                Gizmos.color = Color.green;
+            }
+            else
+            {
+                Gizmos.color = Color.blue;
+                foreach (var subNode in node.Nodes)
+                {
+                    DrawNode(subNode);
+                }
+            }
+            
+            Gizmos.DrawWireCube(node.Position, Vector3.one * node.Size);
+        }
+    }
+}
