75401f8b19a3fa0613d6effba500c6de40eb017f
Ian Barrow <Barrowian1991@gmail.com>
2020-02-06 19:28:24 +0000

    Laser creation using shader graph

---
+++ Other Dimension/Assets/Scripts/Puzzle/Elements/Laser.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Puzzle.Elements
+{
+    public class Laser : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
