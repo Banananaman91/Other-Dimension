58424067710d0a0ebee575004966b1c6fc23398d
Ian <Barrowian1991@gmail.com>
2020-02-12 16:35:45 +0000

    Animation testing

---
+++ Other Dimension/Assets/Prefabs/Animation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    public Animator an1;
+    public GameObject player;
+    public GameObject targetPos;
+    public bool playerClose = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // player detection\\ //.setTrigger\\
+
+
+        if(player.transform.position == targetPos.transform.position)
+        {
+            an1.SetTrigger("player"); 
+        }
+
+
+    }
+}
---
+++ Other Dimension/Assets/Scripts/Puzzle/Elements/Resizer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resizer : MonoBehaviour
+{
+    [SerializeField] private Transform _transform;
+
+
+}
